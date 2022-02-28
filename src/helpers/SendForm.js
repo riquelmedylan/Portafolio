3fcf19598249f3b703f89953ffd4279c3624df89
riquelmedylan <riquelmedylan@hotmail.com>
2022-02-28 14:22:52 -0300

    axios

--- src/helpers/SendForm.js
+++ src/helpers/SendForm.js
@@ -1,16 +1,7 @@
 import axios from "axios";
 export const sendForm = (email, text) => {
      axios.post("https://send-form-rad.herokuapp.com/send-email", {
-          mode: "no-cors",
-          headers: {
-               "Access-Control-Allow-Origin": "*",
-               "Access-Control-Allow-Methods":
-                    "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-               origin: "x-requested-with",
-               "Access-Control-Allow-Credentials": true,
-               Accept: "application/json",
-               "Content-Type": "application/json",
-          },
+
           data: { email, text },
      });
 };
